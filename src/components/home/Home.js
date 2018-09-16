ea13c47cf0fbec98dcc529ab4ca13747c4adaac0
Laassari <nlaassari@gmail.com>
2018-09-17 00:52:10 +0100

    get user state from redux

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import propTypes from 'prop-types'
 import Landing from './Landing'
 import './Home.css'
 
 export class Home extends React.Component {
-  state = {
-    loggedIn: false,
-  }
   render() {
+    const { loggedIn } = this.props.user
     return (
       <div className="home-wrapper">
-        <div className="home">
-          {this.state.loggedIn ? <p>Hello User</p> : <Landing />}
-        </div>
+        <div className="home">{loggedIn ? <p>Hello User</p> : <Landing />}</div>
       </div>
     )
   }
 }
 
-export default Home
+Home.propTypes = {
+  user: propTypes.object.isRequired,
+}
+
+const mapPropToState = state => ({
+  user: state.user,
+})
+
+export default connect(mapPropToState)(Home)
