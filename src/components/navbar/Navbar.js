183a080941682aba3df2df55b7a15d64abe411f0
Laassari <nlaassari@gmail.com>
2018-09-17 00:51:57 +0100

    get user state from redux

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import propTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import UserAction from './UserActions'
 import logo from './logo.svg'
 import './Navbar.css'
 
 export class Navbar extends React.Component {
-  state = {
-    loggedIn: true,
-  }
-
   render() {
+    const { loggedIn } = this.props.user
     return (
       <div className="nav-wrapper">
         <nav>
@@ -19,11 +18,7 @@ export class Navbar extends React.Component {
             </Link>
           </div>
           <div className="login">
-            {this.state.loggedIn ? (
-              <UserAction />
-            ) : (
-              <Link to="/auth">Login</Link>
-            )}
+            {loggedIn ? <UserAction /> : <Link to="/auth">Login</Link>}
           </div>
         </nav>
       </div>
@@ -31,4 +26,12 @@ export class Navbar extends React.Component {
   }
 }
 
-export default Navbar
+Navbar.propTypes = {
+  user: propTypes.object.isRequired,
+}
+
+const mapPropToState = state => ({
+  user: state.user,
+})
+
+export default connect(mapPropToState)(Navbar)
