a5ec84c2f1a239c8cba9cd8646ea19e542ee5459
Laassari <nlaassari@gmail.com>
2018-09-18 12:24:30 +0100

    redirect if user logged In store data if not

--- src/components/user/Auth.js
+++ src/components/user/Auth.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import firebase from './Firebase'
+import { connect } from 'react-redux'
+import propTypes from 'prop-types'
+import { addUserToDb, setUser } from '../../actions/userActions'
 import 'firebase/auth'
 import './Auth.css'
 
 const Auth = props => {
+  if (props.user.loggedIn) {
+    props.history.push('/')
+    return null
+  }
+
   const uiConfig = {
     callbacks: {
-      signInSuccessWithAuthResult() {
-        //TODO: update the redux store.
-        props.history.push('/')
+      signInSuccessWithAuthResult(authResult) {
+        const user = {
+          username:
+            authResult.user.displayName ||
+            authResult.additionalUserInfo.username, //for Github
+
+          id: authResult.user.uid,
+          loggedIn: true,
+          avatarUrl: authResult.user.photoURL,
+        }
+
+        if (authResult.additionalUserInfo.isNewUser) {
+          //sign up
+          props.addUserToDb(user).then(() => props.history.push('/'))
+        } else {
+          //returning user
+          props.setUser(user)
+          props.history.push('/')
+        }
       },
     },
     signInFlow: 'popup',
@@ -25,7 +49,6 @@ const Auth = props => {
     <div className="auth">
       <p>Please sign-in:</p>
       <StyledFirebaseAuth
-        {...props}
         routeHistory={props.history}
         uiConfig={uiConfig}
         firebaseAuth={firebase.auth()}
@@ -34,4 +57,14 @@ const Auth = props => {
   )
 }
 
-export default Auth
+Auth.propTypes = {
+  addUserToDb: propTypes.func.isRequired,
+  setUser: propTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({ user: state.user })
+
+export default connect(
+  mapStateToProps,
+  { addUserToDb, setUser }
+)(Auth)
