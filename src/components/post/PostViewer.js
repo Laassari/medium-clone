4a81ea906ad812f80add238856d9cd78cf1dd2c4
Laassari <nlaassari@gmail.com>
2018-09-23 17:57:50 +0100

    initial commit

---
+++ src/components/post/PostViewer.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import 'froala-editor/css/froala_style.min.css'
+import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView'
+
+import { db } from '../user/Firebase'
+import './PostViewer.css'
+
+class PostViewer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFetching: false,
+      notFound: false,
+      error: false,
+      title: '',
+      body: '',
+      date: '',
+      avatarUrl: false,
+      username: false,
+    }
+  }
+
+  componentDidMount() {
+    const { postId } = this.props.match.params
+    this.setState(() => ({ isFetching: true }))
+
+    const postRef = db.collection('posts').doc(postId)
+
+    postRef
+      .get()
+      .then(async doc => {
+        if (!doc.exists) {
+          this.setState(() => ({ notFound: true, isFetching: false }))
+        } else {
+          const data = doc.data()
+          const { authorId } = data
+          const authorData = await db
+            .collection('users')
+            .doc(authorId)
+            .get()
+
+          //author data
+          const { avatarUrl, username: authorName } = authorData.data()
+          this.setState(() => ({ avatarUrl, authorName }))
+
+          //post data
+          const { title, body } = data
+          const date = new Date(data.date).toLocaleString()
+          this.setState(() => ({ title, body, date, isFetching: false }))
+        }
+      })
+      .catch(err =>
+        this.setState(() => ({
+          error: 'an error occured please try later!',
+          isFetching: false,
+        }))
+      )
+  }
+
+  render() {
+    const {
+      isFetching,
+      error,
+      notFound,
+      title,
+      body,
+      date,
+      avatarUrl,
+      authorName,
+    } = this.state
+
+    return (
+      <div className="post-viewer">
+        {isFetching && <div>Loading... </div>}
+
+        {error && <div>{error}</div>}
+        {notFound && <div>Post not found!</div>}
+
+        {body.length > 0 ? (
+          <div className="post">
+            <img src={avatarUrl} alt="author avatar" />
+            author: {authorName}
+            <span>{date}</span>
+            <h1>{title}</h1>
+            <FroalaEditorView model={body} />
+          </div>
+        ) : (
+          false
+        )}
+      </div>
+    )
+  }
+}
+
+export default PostViewer
